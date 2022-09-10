6c083306cb7502624782a0f21142475100dbdd39
BayodeOg <bayodeog@gmail.com>
2022-09-10 11:21:51 +0100

    hello - C

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Print Hello, welcome to the world of C.
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	printf("Hello, welcome to the world of C\n");
+	return(0);
+}
